0956a9b06a821b327a918f35e17d387377350b54
Nguyen Thanh Trieu <trieunguyenthanh@admicro.vn>
2020-07-24 15:02:17 +0700

    update code

--- redux/saga/index.js
+++ redux/saga/index.js
@@ -1,9 +1,8 @@
 import {call, all} from 'redux-saga/effects';
 import { watchGetDataWeatherSaga } from './weatherSaga';
 
-const rootSaga = () => {
+export default function* rootSaga()  {
   yield all([
     call(watchGetDataWeatherSaga)
   ])
 }
-export default rootSaga;
